ee3adddf7e302e3dba991ae213f23be3384207ad
Faith <fnbiketi@gmail.com>
2021-12-15 04:14:10 +0300

    ..

---
+++ color project/index.js
@@ -0,0 +1,9 @@
+//dom element
+//function that will create colors
+//colors that will be there on change
+function changeColor(){
+  //arrays of colors
+const colors=['blue','red','yellow','green','pink','brown','grey'];
+Math.floor(Math.random() * colors);
+
+}
