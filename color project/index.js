7234a81c6d94b92213a7434c9c6737938d84216e
Faith <fnbiketi@gmail.com>
2021-12-25 19:22:21 +0300

    Background Color

--- color project/index.js
+++ color project/index.js
@@ -1,9 +1,16 @@
-//dom element
-//function that will create colors
-//colors that will be there on change
+//change background colors
 function changeColor(){
-  //arrays of colors
-const colors=['blue','red','yellow','green','pink','brown','grey'];
-Math.floor(Math.random() * colors);
+//create arrays of colors that should be displayed on the screen.
+const colors=['blue','red','yellow','green','pink','brown','grey','orange'];
+//variable that picks color randomly when bg color is  changed
+let color=Math.floor(Math.random() * colors.length);
+color=colors[color]
+data=document.getElementById('click').style.backgroundColor = color;
+console.log(data)
+return data;
 
 }
+
+
+
+
